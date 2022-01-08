b2b6079d8c30a699b3d3fc2f47383357e029cbe4
balaramH <balaramhalder56@gmal.com>
2022-01-08 12:41:58 +0530

    my first code

---
+++ reg.js
@@ -0,0 +1,4 @@
+let quit = "qu";
+let noquit = "qt";
+let quRegex = "/q(?=u)/";
+let quNregex = "/q(?=u)/";
---
+++ tryImport.js
@@ -0,0 +1,12 @@
+//top level import  // like write code(static)
+import { add, sub } from './index.js'
+console.log(add(1, 2), sub(5, 3));
+
+const k = 10;
+
+// conditional import //like optional use part(dynamic)
+if (k < 20) {
+    import ('./index.js').then(({ add, sub }) => {
+        console.log(add, sub);
+    });
+}
